867601f516b6e6542f0bfb757b1d9346a69109a2
Flávio Félix <fau_felix30@hotmail.com>
2022-02-14 20:40:11 -0300

    Realizado as aulas sobre operadores menor e maior e and lógico

---
+++ 5_oper_e_expressoes/22_exercicio_17/index.php
@@ -0,0 +1,14 @@
+<?php
+
+if(15 > 5 && "Joao" === "Joao") {// True e true = true
+  echo "Entrou no if 1<br>";
+}
+if("teste" > 5 && 1) {  
+  echo "Entrou no if 2<br>";
+}
+if(10 > 5 && 1) {  
+  echo "Entrou no if teste<br>";
+}
+if(2 == 3 && 5 >= 3) {
+  echo "Entrou no if 3<br>";
+}
