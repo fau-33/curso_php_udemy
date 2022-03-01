c75fc217a66bbbc723e0413f16999e3a8ab39640
Flávio Félix <fau_felix30@hotmail.com>
2022-02-28 22:02:54 -0300

    Estudada op de atribuição e ternário e feito o ex 20

---
+++ 5_oper_e_expressoes/29_op_ternario/index.php
@@ -0,0 +1,15 @@
+<?php
+// Deu true
+echo 20 > 10 ? "Deu true <br>": "Deu false <br>";
+
+// Deu false
+echo 20 > 50 ? "Deu true <br>": "Deu false <br>"; 
+
+$a = 10;
+$b = 5;
+// Deu true
+echo $a >= $b ? "Deu true <br>": "Deu false <br>";
+// Deu false
+echo $a === $b ? "Deu true <br>": "Deu false <br>";
+
+echo $a === $b && 10 > 5 ? "Deu true <br>": "Deu false <br>";
---
+++ 5_oper_e_expressoes/30_exercicio_20/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = 20;
+$b = 10;
+
+// true
+echo $a <= $b ? "A é menor ou igual a B<br>":"B é menor igua a A <br>";
+// false
+echo $b <= $a ? "A é menor ou igual a B<br>":"B é menor igua a A <br>";
