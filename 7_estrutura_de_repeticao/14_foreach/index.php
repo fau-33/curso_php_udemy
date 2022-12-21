f8e2ffc811e574ebaa58c4df38b36a6094365e63
Flávio Félix <fau_felix30@hotmail.com>
2022-12-21 18:20:16 -0300

    Feito os exemplos sobre loop infinito e foreach array

---
+++ 7_estrutura_de_repeticao/14_foreach/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nomes = ["Matheus", "Maria", "Flávio", "Valéria"];
+
+foreach($nomes as $nome) {
+  echo "O nome do índice atual: $nome <br/>";
+
+  if ($nome == "Matheus") {
+    echo "Xablau PHP!! <br/>";
+  }
+}
